c05c9695581111a63ad6d71da5a618a8af762c53
kshelton7821 <keaton.shelton2@gmail.com>
2022-08-25 15:47:03 -0500

    More Cleanup
    Start Text Dev

---
+++ emailSystem/emailClient/textClientMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using Email_Client;
+
+namespace Text_Client
+{
+    class TextPackage
+    {
+        //Text Message Test Function
+        public static void sendText(MailAddress from, string number, string password, string subject, string body, string host, int port)
+        {
+            string cricket = "@mms.cricketwireless.net";
+            MailAddress too = new MailAddress(number + cricket, "Keaton Shelton");
+            //Test Send to me on Cricket
+            MailPackage.sendMail(from, too, password, subject, body, host, port);
+        }
+    }
+}
