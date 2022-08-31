6da51c4b6836121ef6675933874b179f5bd5271d
kshelton7821 <keaton.shelton2@gmail.com>
2022-08-31 18:34:06 -0500

    Begin Gateway Array Setup

--- emailSystem/emailClient/textClientFunc.cs
+++ emailSystem/emailClient/textClientFunc.cs
@@ -11,6 +11,7 @@
  *  Revisions:
  *  01ks - August 25th, 2022 - Original
  *  02ks - August 29th, 2022 - Add in MMS support
+ *  03ks - August 31st, 2022 - Begin Gateway Array Setup
  */
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,27 @@ namespace Text_Client
 {
     class TextPackage
     {
+        //Global Variables
+        //SMS / MMS String Array
+        /*
+         *1. T-Mobile: @tmomail.net sms/mms
+         *2. AT&T: @txt.att.net (sms) @mms.att.net (mms)
+         *3. Verizon: @vtext.com (sms) @vzwpix.com (mms)
+         *4. Virgin: @vmobl.com (sms) @vmpix.com (mms)
+         *5. Boost: @sms.myboostmobile.com (sms) @myboostmobile.com (mms)
+         *6. Xfinity: @vtext.com (sms) @mypixmessages.com (mms)
+         *7. Sprint: @messaging.sprintpcs.com (sms) @pm.sprint.com
+         *8. U.S. Cell: @email.uscc.net (sms) @mms.uscc.net (mms)
+         *9. Cricket: @sms.cricketwireless.net (sms) @mms.cricketwireless.net
+         *10. Tracfone: @mmst5.tracfone.com sms/mms
+         *11. Metro: @mymetropcs.com sms/mms
+         *12. Ting: @message.ting.com sms/mms
+         */
+        public string[] sms = {"@tmomail.net", "@txt.att.net", "@vmobl.com", "@sms.myboostmobile.com", "@vtext.com", "@messaging.sprintpcs.com",
+            "@email.uscc.net", "@sms.circketwireless.net", "@mmst5.tracfone.com", "@mymetropcs.com", "@message.ting.com"};
+
+
+
         //Normal Text Message Test Function
         public static void sendText(MailAddress from, string number, string password, string subject, string body, string host, int port)
         {
