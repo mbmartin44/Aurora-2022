ea8d5e76d89704f987ac9877e95260cf4629509d
Blake Martin <mbmartin44@tntech.edu>
2022-10-13 16:43:14 -0500

    Aurora-54: Added Unity functionality for automatically adding subsystem csprojs to generated solution file. +semver:patch

---
+++ UnityProject/Assets/Scripts/ChannelsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Neuro;
+using Neuro.Native;
+using UnityEngine;
+
+public class ChannelsController
+{
+    #region Battery
+    Battery batteryController = null;
+    public void createBattery(Device device, Action<int> onPowerChanged)
+    {
+        batteryController = new Battery(device);
+        batteryController.onPowerChanged = onPowerChanged;
+    }
+
+    public void destroyBattery() {
+        batteryController.CloseChannel();
+    }
+    #endregion
+
+    #region Signal
+    SignalController signalController = null;
+    public void createSignal(Device device, EventHandler<double[]> onSignalRecieved)
+    {
+        signalController = new SignalController(device);
+        signalController.onSignalRecieved = onSignalRecieved;
+    }
+
+    public int GetSignalPlotSize()
+    {
+        return signalController.plotSize;
+    }
+
+    public void destroySignal(Device device)
+    {
+        signalController.CloseChannels(device);
+    }
+    #endregion
+
+    #region Resistance
+    ResistanceController resistanceController = null;
+    public void createResistance(Device device, EventHandler<double> onResistRecieved)
+    {
+        resistanceController = new ResistanceController(device);
+        resistanceController.onResistChanged = onResistRecieved;
+    }
+
+    public void destroyResistance(Device device)
+    {
+        resistanceController.CloseChannel(device);
+    }
+
+    #endregion
+
+    #region EEG
+    EegController eegController = null;
+    public void createEeg(Device device, EventHandler<double[]> onEegChanged)
+    {
+        eegController = new EegController(device);
+        eegController.onEegChanged = onEegChanged;
+    }
+
+    public int GetEegPlotSize()
+    {
+        return eegController.plotSize;
+    }
+
+    public void destroyEeg(Device device)
+    {
+        eegController.CloseChannel(device);
+    }
+    #endregion
+
+    #region EegIndex
+    EegIndexController indexController = null;
+    public void createEegIdx(Device device, Action<EegIndexValues> onEegIdxChanged) 
+    {
+        indexController = new EegIndexController(device);
+        indexController.onEegIdxChanged = onEegIdxChanged;
+    }
+
+    public void destroyEegIdx(Device device) 
+    {
+        indexController.CloseChannel(device);
+    }
+    #endregion
+
+    #region Spectrum
+    SpectrumController spectrumController = null;
+    public void createSpectrum(Device device, EventHandler<double[]> onSpectrumChanged) 
+    {
+        spectrumController = new SpectrumController(device);
+        spectrumController.onSpectrumChanged = onSpectrumChanged;
+    }
+
+    public int GetSpectrumPlotSize()
+    {
+        return spectrumController.plotSize;
+    }
+
+    public void destroySpectrum(Device device)
+    {
+        spectrumController.CloseChannel(device);
+    }
+    #endregion
+
+    #region SpectrumPower
+    SpectrumPowerController spectrumPowerController = null;
+    public void createSpectrumPower(Device device, EventHandler<double> onSpectrumPowerChanged) 
+    {
+        spectrumPowerController = new SpectrumPowerController(device);
+        spectrumPowerController.onSpectrumPowerChanged += onSpectrumPowerChanged;
+    }
+
+    public void destroySpectrumPower(Device device)
+    {
+        spectrumPowerController.CloseChannel(device);
+    }
+    #endregion
+
+    #region EmotionAnalyzer
+    EmotionsController emotionsController = null;
+    public void createEmotions(Device device, Action<EmotionsSample> onEmotionalStateChanged, Action<IEmotionsAnalyzerState> onAnalyzerStateChanged)
+    {
+        emotionsController = new EmotionsController(device);
+        emotionsController.onEmotionalStateChanged += onEmotionalStateChanged;
+        emotionsController.onAnalyzerStateChanged += onAnalyzerStateChanged;
+    }
+
+    public void destroyEmotions(Device device)
+    {
+        emotionsController.CloseChannel(device);
+    }
+    #endregion
+}
---
+++ UnityProject/Assets/Scripts/Graph.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Graph : MonoBehaviour
+{
+    public RectTransform container;
+    public RectTransform background;
+
+    protected int samplesCount = 0;
+    protected List<RectTransform> connections = new List<RectTransform>();
+
+    public virtual void InitGraph(int samplesCount)
+    {
+        this.samplesCount = samplesCount;
+    }
+
+    /*public void InitGraph(int samplesCount)
+    {
+        this.samplesCount = samplesCount;
+
+        for (int i = 0; i < samplesCount; i++)
+        {
+            samplesQueue.Enqueue(0);
+        }
+        ShowGraph(samplesQueue.ToArray());
+    }*/
+
+
+    protected virtual void ShowGraph(double[] samples)
+    {
+        container.sizeDelta = new Vector2(background.rect.width, background.rect.height);
+        container.anchoredPosition = new Vector2(container.sizeDelta.x * 0.5f, container.sizeDelta.y * 0.5f);
+        /*float graphHeight = container.sizeDelta.y;
+        float yMax = 10000f;
+        float xSize = container.sizeDelta.x / samplesCount;
+
+        float lastX = -1;
+        float lastY = -1;
+        for (int i = 0; i < samples.Length; i++)
+        {
+            float xPosition = xSize + i * xSize;
+            float yPosition = 0;
+            if (mode == GraphMode.Signal)
+            {
+                yPosition = graphHeight / (yMax * 2.0f) * ((float)samples[i] + yMax);
+            }
+            else
+            {
+                yPosition = (float)samples[i] * graphHeight / yMax;
+            }
+            yPosition = Mathf.Clamp(yPosition, 0, graphHeight);
+
+            if (lastX > -1 && lastY > -1) {
+                connections.Add(CreateConnection(new Vector2(lastX, lastY),
+                    new Vector2(xPosition, yPosition)));
+            }
+            lastX = xPosition;
+            lastY = yPosition;
+        }*/
+    }
+
+    protected RectTransform CreateConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("connection", typeof(Image));
+        gameObject.transform.SetParent(container, false);
+        gameObject.GetComponent<Image>().color = Color.black;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        SetConnectionSizes(rectTransform, dotPositionA, dotPositionB);
+        return rectTransform;
+    }
+
+    /*private void FixedUpdate()
+    {
+        double[] samples = samplesQueue.ToArray();
+
+        float graphHeight = container.sizeDelta.y;
+        float yMax = 10000f;
+        float xSize = container.sizeDelta.x / samplesCount;
+
+        float lastX = -1;
+        float lastY = -1;
+        for (int i = 0; i < samples.Length - 1; i++)
+        {
+            float xPosition = xSize + i * xSize;
+            float yPosition = 0;
+            if (mode == GraphMode.Signal)
+            {
+                yPosition = graphHeight / (yMax * 2.0f) * ((float)samples[i] + yMax);
+            }
+            else
+            {
+                yPosition = (float)samples[i] * graphHeight / yMax;
+            }
+            yPosition = Mathf.Clamp(yPosition, 0, graphHeight);
+
+            if (lastX > -1 && lastY > -1 && i < connections.Count)
+            {
+                UpdateConnection(new Vector2(lastX, lastY), new Vector2(xPosition, yPosition), i);
+            }
+            lastX = xPosition;
+            lastY = yPosition;
+        }
+    }*/
+
+    public virtual void UpdateGraph(double[] newSamples)
+    {
+        /*for (int i = 0; i < newSamples.Length; i++)
+        {
+            if (samplesQueue.Count < samplesCount)
+            {
+                samplesQueue.Enqueue(newSamples[i]  * 1e6);
+            }
+            else
+            {
+                samplesQueue.Enqueue(newSamples[i] * 1e6);
+                samplesQueue.Dequeue();
+            }
+        }*/
+
+    }
+
+    protected void UpdateConnection(Vector2 dotPositionA, Vector2 dotPositionB, int connectionId)
+    {
+        SetConnectionSizes(connections[connectionId], dotPositionA, dotPositionB);
+    }
+
+    private void SetConnectionSizes(RectTransform rectTransform, Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 1f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(dir));
+    }
+
+    private float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+    public virtual void Close()
+    {
+        //samplesQueue.Clear();
+        connections.ForEach((connection) => { Destroy(connection.gameObject); });
+        connections.Clear();
+    }
+}
