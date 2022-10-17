4541a72862d27ee3a029c3489c09b79c9c47b695
kshelton7821 <keaton.shelton2@gmail.com>
2022-10-17 02:24:00 -0500

    Break apart name space into small class files

--- UnityProject/Subsystems/EmailClientLib/clientInterface.cs
+++ UnityProject/Subsystems/EmailClientLib/clientInterface.cs
@@ -12,6 +12,9 @@
  *    The primary function will execute the bulk work of the email client and text
  *    client. A small started function will be used to watch for a seizure positive signal
  *    and execute the primary function on a true condition.
+ *    
+ * Revisions:
+ * 01ks - October 17th, 2022 - Name Fix, remove namespace, remove supporting classes
 */
 using System;
 using System.Text;
@@ -21,187 +24,46 @@ using System.Net;
 using System.Net.Mail;
 using UnityEngine;
 
-namespace PrimaryInterface
+
+class ClientInterface : MonoBehaviour
 {
-    public class ClientInterface : MonoBehaviour
+    //Main Use Function
+    public static async void SignalWatch(List<ContactsPackage> people, bool detect, Attachment attach)
     {
-        //Main Use Function
-        public static async void SignalWatch(List<ContactsPackage> people, Attachment attach, bool detect, bool validAtt)
+        if (detect)
         {
-            if (detect)
+            //Send emails and text messages from contacts list
+            foreach (var x in people)
             {
-                var fromAddress = new MailAddress("brainanalytics2022@gmail.com", "Seizure App Auto Sender");
-                const string fromPassword = "xgfxsygrzzcenjlv";
-                const string host = "smtp.gmail.com";
-                const int port = 587;
-                string subject = "Event Detected";
-                string body = "This is an automated message from Seizure Detection App that an event has been recorded";
-
-                //Send emails and text messages from contacts list
-                foreach (var x in people)
-                {
-                    if (validAtt)
-                    {
-                        TextDriverFunc texter = new TextDriverFunc();
-                        texter.Send(x);
-                        MailPackage.sendMailAttach(fromAddress, x, fromPassword, subject, body, host, port, attach);
-                        await System.Threading.Tasks.Task.Delay(2000);
-                    }
-                    else
-                    {
-                        TextDriverFunc texter = new TextDriverFunc();
-                        texter.Send(x);
-                        MailPackage.sendMail(fromAddress, x, fromPassword, subject, body, host, port);
-                        await System.Threading.Tasks.Task.Delay(2000);
-
-                    }
-                }
-            }
-            else
-            {
-                return;
+                TextDriver texter = new TextDriver();
+                texter.Send(x);
+                MailPackage.sendMailAttach(x, attach);
+                await System.Threading.Tasks.Task.Delay(2000);
             }
         }
-
-        public async void SignalWatchBasic(List<ContactsPackage> people, bool detect)
+        else
         {
-            if (detect)
-            {
-                var fromAddress = new MailAddress("brainanalytics2022@gmail.com", "Seizure App Auto Sender");
-                const string fromPassword = "xgfxsygrzzcenjlv";
-                const string host = "smtp.gmail.com";
-                const int port = 587;
-                string subject = "Event Detected";
-                string body = "This is an automated message from Seizure Detection App that an event has been recorded";
-
-
-
-                //Send emails and text messages from contacts list
-                foreach (var x in people)
-                {
-                    TextDriverFunc texter = new TextDriverFunc();
-                    texter.Send(x);
-                    MailPackage.sendMail(fromAddress, x, fromPassword, subject, body, host, port);
-                    await System.Threading.Tasks.Task.Delay(2000);
-
-                }
-            }
-            else
-            {
-                return;
-            }
+            return;
         }
     }
 
-    //Contacts Class
-    public class ContactsPackage
+    public static async void SignalWatchBasic(List<ContactsPackage> people, bool detect)
     {
-        public MailAddress nameAddress { get; set; }
-        public string phone { get; set; }
-    }
-
-    //Email Class
-    public class MailPackage
-    {
-        //Email with attachment
-        public static void sendMailAttach(MailAddress from, ContactsPackage too, string password, string subject, string body, string host, int port, Attachment attach)
+        if (detect)
         {
-            try
+            //Send emails and text messages from contacts list
+            foreach (var x in people)
             {
-                using (var smtp = new SmtpClient(host))
-                {
-                    smtp.Host = host;
-                    smtp.Port = port;
-                    smtp.EnableSsl = true;
-                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new NetworkCredential(from.Address, password);
+                TextDriver texter = new TextDriver();
+                texter.Send(x);
+                MailPackage.sendMail(x);
+                await System.Threading.Tasks.Task.Delay(2000);
 
-                    using (var message = new MailMessage(from, too.nameAddress))
-                    {
-                        message.Subject = subject;
-                        message.Body = body;
-                        message.Attachments.Add(attach);
-                        smtp.Send(message);
-                    };
-                };
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError("Error: " + e.ToString());
             }
         }
-
-
-        //Email with no attachment
-        public static void sendMail(MailAddress from, ContactsPackage too, string password, string subject, string body, string host, int port)
+        else
         {
-            try
-            {
-                using (var smtp = new SmtpClient(host))
-                {
-                    smtp.Host = host;
-                    smtp.Port = port;
-                    smtp.EnableSsl = true;
-                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new NetworkCredential(from.Address, password);
-
-                    using (var message = new MailMessage(from, too.nameAddress))
-                    {
-                        message.Subject = subject;
-                        message.Body = body;
-                        smtp.Send(message);
-                    };
-                };
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError("Error: " + e.ToString());
-            }
-        }
-    }
-
-    //Text Class
-    public class TextDriverFunc : MonoBehaviour
-    {
-        AndroidJavaObject currentActivity;
-        ContactsPackage current;
-
-        public void Send(ContactsPackage person)
-        {
-            if (Application.platform == RuntimePlatform.Android)
-            {
-                RunAndroidUiThread();
-                current = person;
-            }
-        }
-
-        void RunAndroidUiThread()
-        {
-            AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-            currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(SendProcess));
-        }
-
-        void SendProcess()
-        {
-            AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
-
-            string phone = current.phone;
-            string text = "Hello there general kenobi";
-
-            try
-            {
-                AndroidJavaClass SMSManagerClass = new AndroidJavaClass("android.telephony.SmsManager");
-                AndroidJavaObject SMSManagerObject = SMSManagerClass.CallStatic<AndroidJavaObject>("getDefault");
-                SMSManagerObject.Call("sendTextMessage", phone, null, text, null, null);
-            }
-            catch (System.Exception e)
-            {
-                Debug.Log("Error : " + e.StackTrace.ToString());
-            }
+            return;
         }
     }
 }
-
