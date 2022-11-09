be986abf5cb636f15a89f38f4e0246ee2603a3ab
kshelton7821 <keaton.shelton2@gmail.com>
2022-11-08 19:57:34 -0600

    Issue96 - ContactIO backend completed. Frontend needs to be done.

---
+++ UnityProject/Subsystems/EmailClientLib/ContactsList.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Name: Keaton Shelton
+ * Date: November 8th, 2022
+ * Arguments: A List<ContactsPackage>
+ * Returns: A List<ContactsPackage>
+ * 
+ * Abstract:
+ *      This class is an intermidate class for use with ContactsIO and ContactsPackage.
+ *      This class is a Serializable List of ContactsPackages' to make IO easier.
+ *      
+ * Revisions:
+ * 01ks - November 8th, 2022 - Original
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+public class ContactsList
+{
+    public List<ContactsPackage> List;
+
+    public ContactsList()
+    {
+        //Dont Use unless loading contacts
+        this.List = new List<ContactsPackage>();
+    }
+    public ContactsList(List<ContactsPackage> input)
+    {
+        this.List = new List<ContactsPackage>();
+        this.List = input;
+    }
+}
+
